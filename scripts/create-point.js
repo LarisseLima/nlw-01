e605ee1336fa8527121fccb5b3d3cd635e677a62
Larisse Lima <larisse.lima2@gmail.com>
2020-06-03 00:36:12 -0300

    Ajustes

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,7 +1,7 @@
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
         .then(res => res.json())
         .then(states => {
             for (const state of states) {
